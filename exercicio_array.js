3c13be7f4efcc904181940b0f33df8d74972a993
YuYuGOD <yuri_wendt@hotmail.com>
2020-02-16 20:02:14 -0300

    Exercicio Array

---
+++ exercicio_array.js
@@ -0,0 +1,23 @@
+// Comentar usa // ou /*
+
+const usuario = {
+    
+    nome: "Diego",     
+    empresa: {
+    nome: "Rocketseat",
+    cor: "Roxo",
+    foco: "Programaçao",
+    endereco : {
+        rua: "Rua Guilherme Gembala",
+        numero: "260",
+       }
+    }
+};
+
+console.log(' A empresa ' + usuario.empresa.nome + ' está localizada em ' + usuario.empresa.endereco.rua + usuario.empresa.endereco.numero);
+
+
+
+
+
+
