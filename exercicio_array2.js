e5222b473f04b98b7d5e93cdd4c787697cd72c51
YuYuGOD <yuri_wendt@hotmail.com>
2020-02-16 20:34:20 -0300

    Exercicio Array 2

---
+++ exercicio_array2.js
@@ -0,0 +1,21 @@
+// Comentar usa // ou /*
+
+const usuario = {
+    nome: "Diego",
+    idade: 32,
+}
+
+const objeto = {
+    propriedade: [
+        {nome: "C++", especialidade: "Desktop"},
+        {nome: "JavaScript", especialidade: "Web/Mobile"}
+    ]
+}
+
+console.log("O usuario " + usuario.nome + " tem " + usuario.idade + " e usa a tecnologia " + objeto.propriedade[1].nome + " com " + objeto.propriedade[1].especialidade);
+
+
+
+
+
+
